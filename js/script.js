1bfbfdab625b4c344b9e560d16c506be1e7854ed
Derrick Cudd <instantramen125@gmail.com>
2017-01-05 03:41:36 -0500

    created a 16*16 grid for drawing

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  // create a 16*16 grid of divs
+  for(x = 0; x < 16; x++) {
+    for(y = 0; y < 16; y++) {
+      $('#container').append("<div class='grid-block " + x + "-" + y + "' ></div>");
+    }
+  }
+
+});
