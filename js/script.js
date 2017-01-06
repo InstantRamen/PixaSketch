4ffdcaaee3fc372053970feabfab6652f9152510
Derrick Cudd <instantramen125@gmail.com>
2017-01-06 01:01:29 -0500

    bug fixes(it actually works now) and cleaning up code

--- js/script.js
+++ js/script.js
@@ -1,17 +1,13 @@
-var gridWidth = 100;
-var gridHeight = 100;
-var blockWidth = 100/gridWidth;
+var initGridSize = 32;
 $(document).ready(function() {
-  // create a 16*16 grid of divs
-  initGrid(gridWidth, gridHeight);
-
+  $loadbar = $("#loading");
+  $grid = $("#grid-container");
+  initGrid(initGridSize, initGridSize);
 });
 
 $(document).on("mouseover", ".grid-block", function() {
   $(this).removeClass("inactive-block");
   $(this).addClass("active-block");
-
-
 });
 
 $(document).on("mouseover", "#clear", function() {
@@ -23,30 +19,26 @@ $(document).on("mouseleave", "#clear", function() {
 });
 
 $(document).on("click", "#clear", function() {
-  $(this).removeClass('hover');
+  var size = prompt("Grid Size: ", 16);
+  if(!(size > 0)) {
+    size = 16;
+  }
+  var blockWidth = 100/gridWidth;
   clearGrid();
-  var width = prompt("Grid Size: ", 16);
+  initGrid(size, size);
 });
 
 function initGrid(width, height) {
   gridWidth = width;
   gridHeight = height;
-
+  var blockWidth = 100/gridWidth;
+  console.log(blockWidth);
   for(x = 0; x < width; x++) {
     for(y = 0; y < height; y++) {
-      $('#grid-container').append("<div class='grid-block " + x + "-" + y + " inactive-block'  style='width:" + blockWidth + "%; height:" + blockWidth + "%;'></div>");
+      $grid.append("<div class='grid-block " + x + "-" + y + " inactive-block'  style='width:" + blockWidth + "%; height:" + blockWidth + "%;'></div>");
     }
   }
-
 }
-
 function clearGrid() {
-  for(x = 0; x < gridWidth; x++) {
-    for(y = 0; y < gridHeight; y++) {
-      console.log(x + "-" + y);
-      $grid = $("." + x + "-" + y);
-      $grid.removeClass("active-block");
-      $grid.addClass("inactive-block");
-    }
-  }
+  $grid.html('');
 }
